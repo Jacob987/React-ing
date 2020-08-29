43c2af06f5efedde5b43d6f20504878f8b4ee74a
Jacob Kim <jkkim1998@gmail.com>
2020-08-29 14:46:32 +1200

    commit

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,7 +3,17 @@ import people from './peopleData'
 import Person from './Person';
 
 function Main() {
-    const peopleComponents = people.map(person => <Person key={person.id} name={person.name} age={person.age}/>)
+    const peopleComponents = people.sort(function (person1, person2) {
+        if (person1.age < person2.age) {
+            return -1
+        }
+        else if (person1.age === person2.age) {
+            return 0;
+        }
+        else {
+            return 1;
+        }
+    }).map(person => <Person key={person.id} name={person.name} age={person.age}/>)
 
     return (
         <div>
--- src/Components/Person.js
+++ src/Components/Person.js
@@ -9,7 +9,7 @@ function Person(props) {
         <div style={style}>
             <h2>{props.name}</h2>
             <p>Age: {props.age}</p>
-            <hr/>
+            <hr style={{width:"50%", borderTop: "10px solid #8c8b8b"}}/>
         </div>
     )
 }
